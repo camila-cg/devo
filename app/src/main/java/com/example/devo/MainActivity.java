54832b698e891dd9bc53db9172f6df4b973ea792
camila.gusmao <camila.gusmao@radixeng.com.br>
2019-03-27 13:54:07 -0300

    Criando lista de exemplo

--- app/src/main/java/com/example/devo/MainActivity.java
+++ app/src/main/java/com/example/devo/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.devo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+//import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @BindView(R.id.lista_pessoas)
+//    public ListView listaPessoas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listaPessoas = findViewById(R.id.lista_pessoas);
+        String[] pessoas = {"Fulano", "Ciclano", "Beltrano"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, pessoas);
+        listaPessoas.setAdapter(adapter);
     }
 }
